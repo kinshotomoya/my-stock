c1d6349421b9ef3a94bf410a29d86d58c4b59747
金正朋也 <mhtnim1023@gmail.com>
2020-11-06 20:55:26 +0900

    convert Future[Option[A]] -> OptionT[Future, A]

--- src/main/scala/domain/repository/StockRepository.scala
+++ src/main/scala/domain/repository/StockRepository.scala
@@ -1,6 +1,7 @@
 package domain.repository
 
 
+import cats.data.OptionT
 import com.jimmoores.quandl.{DataSetRequest, Frequency}
 import domain.model.{QuandlResult, StockCode}
 import repository.Builder
@@ -8,5 +9,5 @@ import repository.Builder
 import scala.concurrent.{ExecutionContext, Future}
 
 trait StockRepository {
-  def getStock(code: StockCode, frequency: Frequency)(implicit ec: ExecutionContext, requestBuilder: Builder[DataSetRequest]): Future[Option[QuandlResult]]
+  def getStock(code: StockCode, frequency: Frequency)(implicit ec: ExecutionContext, requestBuilder: Builder[DataSetRequest]): OptionT[Future, QuandlResult]
 }
--- src/main/scala/repository/StockRepositoryImpl.scala
+++ src/main/scala/repository/StockRepositoryImpl.scala
@@ -3,9 +3,11 @@ package repository
 import java.util.concurrent.TimeUnit
 
 import akka.actor.ActorSystem
+import cats.data.OptionT
 import com.google.inject.{Inject, Singleton}
 import com.jimmoores.quandl.classic.ClassicQuandlSession
 import com.jimmoores.quandl.{DataSetRequest, Frequency}
+import cats.implicits._
 import domain.TimeOutError
 import domain.model.{QuandlResult, StockCode}
 import domain.repository.StockRepository
@@ -16,18 +18,19 @@ import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class StockRepositoryImpl @Inject()(actorSystem: ActorSystem) extends StockRepository {
-  // TODO: cats使う
-  override def getStock(code: StockCode, frequency: Frequency)(implicit ec: ExecutionContext, requestBuilder: Builder[DataSetRequest]): Future[Option[QuandlResult]] = {
+  override def getStock(code: StockCode, frequency: Frequency)(implicit ec: ExecutionContext, requestBuilder: Builder[DataSetRequest]): OptionT[Future, QuandlResult] = {
     val delayed = akka.pattern.after(FiniteDuration(500L, TimeUnit.MILLISECONDS), actorSystem.scheduler)(Future.failed(TimeOutError("timeout error")))
 
-    val result = Future {
+    val future = Future {
       val session: ClassicQuandlSession = ClassicQuandlSession.create()
       val request: DataSetRequest = requestBuilder.build(code, frequency)
       session.getDataSet(request)
     }
 
-    Future.firstCompletedOf(Seq(delayed, result)).map(result => Some(result.convertToDomainObject)).recoverWith{
-      case _ => Future(None)
+    OptionT.liftF{
+      Future.firstCompletedOf(Seq(delayed, future)).map(_.convertToDomainObject)
+    }.recoverWith{
+      case _ => OptionT.fromOption[Future](None)
     }
   }
 }
